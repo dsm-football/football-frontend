4c455061c5c04413e7634fe252b382efa117128e
Kimjimin0807 <jm004471@naver.com>
2021-08-25 16:00:44 +0900

    [FEAT] 헤더 추가

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as S from './style';
+
+const Header = () => {
+  return(
+    <>
+      <S.HeaderWrapper>
+        <p>adsd</p>
+      </S.HeaderWrapper>
+    </>
+  );
+}
+
+export default Header;
