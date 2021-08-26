fec086717f61bd7554b369a654596eaf073b1c24
Kimjimin0807 <jm004471@naver.com>
2021-08-26 22:15:23 +0900

    ⚡️ :: 동호회 관리 페이지 추가

--- src/components/clubManagement/ClubManagement.tsx
+++ src/components/clubManagement/ClubManagement.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import Header from '../header/Header';
+import React from "react";
+import Header from "../header/Header";
+import * as S from "./style";
 
 const ClubManagement = () => {
-  return(
-    <Header></Header>
-  )
-}
+  return (
+    <>
+      <Header></Header>
+      <section>
+        <S.ClubManageBanner>
+          <b>동호회 관리</b>
+        </S.ClubManageBanner>
+        <S.ClubManageList>
+          <ul>
+            <li>매칭관리</li>
+            <li>회원관리</li>
+            <li>동호회정보</li>
+          </ul>
+        </S.ClubManageList>
+        <S.MatchingList>
+          
+        </S.MatchingList>
+      </section>
+    </>
+  );
+};
 
 export default ClubManagement;
