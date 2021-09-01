d67aa1d812653c4122038373f85226bf8b163437
LeeJinWoo <wlsdn5793@gmail.com>
2021-09-01 15:57:31 +0900

    ⚡️ :: 페이지 이동 부분 추가

--- src/components/header/style.tsx
+++ src/components/header/style.tsx
@@ -27,3 +27,25 @@ export const ListContainer = styled.div`
   background-color: white;
   margin-right: 20px;
 `
+
+// pageMoveHeader
+
+export const PageMoveHeaderWrapper = styled(HeaderWrapper)`
+  height: 52px;
+  background-color: #ECECEC;
+  box-sizing: border-box;
+  padding: 0 7%;
+`;
+export const BackPage = styled(ListContainer)`
+  margin: 0;
+  background-color:#FFCFCF;
+`;
+export const PageMoveTitle = styled.div`
+  height: 25px;
+  font-size: 18px;
+  font-weight: 700;
+`;
+
+export const NextPage = styled(PageMoveTitle)`
+  font-weight: 500;
+`;
