c3b0fe87c4a1ba3c9fa3eafe1a38e25a0b9f5859
Kimjimin0807 <jm004471@naver.com>
2021-09-01 23:18:41 +0900

    ⚡️ :: 매칭 카드 추가

---
+++ src/components/common/matchCard/style.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+  width: 210px;
+  height: 136px;
+  border: 1px solid #E0E0E0;
+  border-radius: 5px;
+  margin-right: 9px;
+`
+
+export const CardHeader = styled.div`
+  width: 80%;
+  margin: auto;
+  display: flex;
+  justify-content: space-around;
+
+  & p {
+    font-size: 11pxc;
+  }
+`
