e406dfa3b7af924381b1bd35526fd615f3f4eac1
Kimjimin0807 <jm004471@naver.com>
2021-09-10 23:35:13 +0900

    ⚡️ :: 멤버 관리 추가

--- src/components/clubManagement/memberManagement/memberTr/MemberTr.tsx
+++ src/components/clubManagement/memberManagement/memberTr/MemberTr.tsx
@@ -1,10 +1,15 @@
-import * as S from './style';
+import * as S from "./style";
 
 const MemberTr = () => {
-  return(
+  return (
     <S.MemberContainer>
-      <S.MemberImg/>
-      
+      <S.TdContainer>
+        <S.MemberImg />
+      </S.TdContainer>
+      <S.TdContainer>
+        <S.MemberName>회원이름</S.MemberName>
+        <S.MemberInfo>서울/24/남</S.MemberInfo>
+      </S.TdContainer>
     </S.MemberContainer>
   );
 };
--- src/components/clubManagement/memberManagement/memberTr/style.tsx
+++ src/components/clubManagement/memberManagement/memberTr/style.tsx
@@ -8,9 +8,25 @@ export const MemberContainer = styled.tr`
   margin-top: 10px;
 `
 
+export const TdContainer = styled.td`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
 export const MemberImg = styled.td`
   width: 30px;
   height: 30px;
   background-color: #C4C4C4;
   border-radius: 50%;
+`
+
+export const MemberName = styled.span`
+  font-size: 14px;
+`
+
+export const MemberInfo = styled.span`
+  font-size: 10px;
+  color: #545454;
 `
