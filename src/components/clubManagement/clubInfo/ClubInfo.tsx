68d2c1d795c069e0582ddeeb2ac87cff011b27b2
Kimjimin0807 <jm004471@naver.com>
2021-09-15 23:50:27 +0900

    ⚡️ :: 동호회 정보 수정 페이지 추가

--- src/components/clubManagement/clubInfo/ClubInfo.tsx
+++ src/components/clubManagement/clubInfo/ClubInfo.tsx
@@ -1,12 +1,44 @@
-import * as S from './style';
+import * as S from "./style";
 
 const ClubInfo = () => {
-  return(
-    <S.ProfileContainer>
-      <S.ChangeImgBtn>이미지 변경하기</S.ChangeImgBtn>
-      <S.ProfileImg/>
-    </S.ProfileContainer>
-  )
-}
+  return (
+    <>
+      <S.ProfileContainer>
+        <S.ChangeImgBtn>이미지 변경하기</S.ChangeImgBtn>
+        <S.ProfileImg />
+      </S.ProfileContainer>
+      <S.ClubInfoFixContainer>
+        <S.FixBox>
+          <S.TitleFont>동호회명</S.TitleFont>
+          <S.BoldFixInput>FC어쩌구</S.BoldFixInput>
+        </S.FixBox>
+        <S.FixBox>
+          <S.TitleFont>동호회 소개(100자 이내)</S.TitleFont>
+          <S.IntroFixInput>
+            저희 팀은 한 달에 4번 정도 매치경기를 진행하고 있습니다. 나이
+            상관없이 열정있으신 분들이라면 언제든지 환영합니다. 가입신청은
+            1:1카톡 주세요
+          </S.IntroFixInput>
+        </S.FixBox>
+        <S.FixBox>
+          <S.TitleFont>동호회 SNS</S.TitleFont>
+          <S.FixInput>www.dkjfeksfjkasejfksejfesfkj.dfef</S.FixInput>
+        </S.FixBox>
+        <S.FixBox>
+          <S.TitleFont>동호회장 SNS</S.TitleFont>
+          <S.FixInput>www.dkjfeksfjkasejfksejfesfkj.dfef</S.FixInput>
+        </S.FixBox>
+        <S.FixBox>
+          <S.TitleFont>동호회 지역</S.TitleFont>
+          <S.BoldFixInput>서울시 노원구 하계동</S.BoldFixInput>
+        </S.FixBox>
+        <S.FixBox>
+          <S.TitleFont>동호회 정원(현재 정원:25)</S.TitleFont>
+          <S.BoldFixInput>FC어쩌구 <S.GpsImg/></S.BoldFixInput>
+        </S.FixBox>
+      </S.ClubInfoFixContainer>
+    </>
+  );
+};
 
 export default ClubInfo;
--- src/components/clubManagement/clubInfo/style.tsx
+++ src/components/clubManagement/clubInfo/style.tsx
@@ -29,3 +29,62 @@ export const ProfileImg = styled.div`
   background-color: #C4C4C4;
   transform: translateY(50%);
 `
+
+export const ClubInfoFixContainer = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+  margin-top: 60px;
+`
+
+export const FixBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: 25px;
+`
+
+export const TitleFont = styled.span`
+  font-size: 16px;
+  margin-bottom: 10px;
+`
+
+export const BoldFixInput = styled.span`
+  width: 95%;
+  height: 45px;
+  background-color: #C4C4C4;
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  padding-left: 15px;
+`
+
+export const FixInput = styled.span`
+  width: 95%;
+  height: 45px;
+  background-color: #C4C4C4;
+  font-size: 12px;
+  display: flex;
+  align-items: center;
+  padding-left: 15px;
+`
+
+export const IntroFixInput = styled.span`
+  width: 95%;
+  height: 70px;
+  background-color: #C4C4C4;
+  display: flex;
+  align-items: center;
+  padding-left: 15px;
+  font-size: 12px;
+`
+
+export const GpsImg = styled.div`
+  width: 25px;
+  height: 25px;
+  background-color: black;
+`
