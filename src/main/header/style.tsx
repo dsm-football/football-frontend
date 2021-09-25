267973c2f1fd5a58978155d1591fc478678bdd72
jinhyuk <wlsgur0324@naver.com>
2021-09-25 23:36:19 +0900

    ⚡:: Mainpage 추가

---
+++ src/main/header/style.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  background-color: #C4C4C4;
+  justify-content: space-between;
+  & div{
+    display: flex;
+    align-items: center;
+  }
+`
+
+export const Logo = styled.div`
+  width: 70px;
+  height: 40px;
+  background-color: white;
+  margin-left: 20px;
+`
+
+export const ListContainer = styled.div`
+  width: 24px;
+  height: 24px;
+  background-color: white;
+  margin-right: 20px;
+`
+
+export const Menu = styled.div`
+  width: 24px;
+  height: 24px;
+  left: 316px;
+  background-color: white;
+  margin-right: 20px;
+`
---
+++ src/main/nearmatching/style.tsx
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+
+export const ClubManageBanner = styled.div`
+  width: 100%;
+  height: 170px;
+  background-color: #D6D6D6;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Matching = styled.div`
+  width: 197px;
+  height: 25px;
+  top: 240px;
+  background-color: white;
+  margin-left: 20px;
+  margin-top: 10px;
+  font-weight: 600;
+`
+
+export const Searching = styled.div`
+  width: 320px;
+  height: 33px;
+  margin-top: 10px;
+  margin-left: 21px;
+  align-items: center;
+  background-color: #F2F2F2;
+  justify-content: space-between;
+  & div{
+    display: flex;
+    align-items: center;
+  }
+`
+export const Target = styled.div`
+  width: 24px;
+  height: 24px;
+  background-color: white;
+  margin-left: 10px;
+  margin-top: 4px;
+`
+
+export const Locate = styled.div`
+  height: 19px;
+  left: 26px;
+  top: 3px;
+  margin-left: 7px;
+  font-size: 14px;
+  font-weight: 500;
+`
+
+export const Select = styled.div`
+  height: 19px;
+  top: 8px;
+  margin-left: 110px;
+  font-size: 14px;
+  font-weight: 500;
+`
+
+export const Map = styled.div`
+  width: 320px;
+  height: 177px;
+  top: 32px;
+  background-color: #C4C4C4;
+  & div{
+    display: flex;
+    align-items: center;
+  }
+`
+
+export const Point1 = styled.div`
+  width: 28px;
+  height: 40px;
+  top: 59px;
+  margin-left: 70px;
+  background-color: white;
+`
