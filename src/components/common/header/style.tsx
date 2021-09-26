edff8f498b4c9852318cddd33d25185f5401d1f1
LeeJinWoo <wlsdn5793@gmail.com>
2021-09-26 21:38:34 +0900

    ⚡️ :: pagemoveHeader 스타일 완성

--- src/components/common/header/style.tsx
+++ src/components/common/header/style.tsx
@@ -1,51 +1,51 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const HeaderWrapper = styled.div`
   width: 100%;
   height: 50px;
   display: flex;
   align-items: center;
-  background-color: #C4C4C4;
+  background-color: #c4c4c4;
   justify-content: space-between;
 
-  & div{
+  & div {
     display: flex;
     align-items: center;
   }
-`
+`;
 
 export const Logo = styled.div`
   width: 70px;
   height: 40px;
   background-color: white;
   margin-left: 20px;
-`
+`;
 
 export const ListContainer = styled.div`
   width: 24px;
   height: 24px;
   background-color: white;
   margin-right: 20px;
-`
+`;
 
 // pageMoveHeader
 
 export const PageMoveHeaderWrapper = styled(HeaderWrapper)`
   height: 50px;
-  background-color: #ECECEC;
+  background-color: #1e201d;
   box-sizing: border-box;
   padding: 0 7%;
-`;
-export const BackPage = styled(ListContainer)`
-  margin: 0;
-  background-color:#FFCFCF;
 `;
 export const PageMoveTitle = styled.div`
   height: 25px;
   font-size: 18px;
   font-weight: 700;
+  color: #0bff0b;
+  margin: 0 5%;
 `;
 
-export const NextPage = styled(PageMoveTitle)`
+export const PageMove = styled(PageMoveTitle)`
   font-weight: 500;
+  color: white;
+  margin: 0;
 `;
