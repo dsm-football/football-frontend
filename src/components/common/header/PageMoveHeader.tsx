db4d5739e82ed7dd1781b22a0eb9d2252b3b70de
LeeJinWoo <wlsdn5793@gmail.com>
2021-09-29 09:22:06 +0900

    ⚡️ :: pagemoveHeader 스타일 수정

--- src/components/common/header/PageMoveHeader.tsx
+++ src/components/common/header/PageMoveHeader.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import * as S from "./style";
-import { Line } from "../../../assets/index";
 const PageMoveHeader = () => {
   return (
     <S.PageMoveHeaderWrapper>
-      <S.PageMove>이전</S.PageMove>
-      <img src={Line} alt="" />
       <S.PageMoveTitle>동호회 개설</S.PageMoveTitle>
-      <img src={Line} alt="" />
-      <S.PageMove>다음</S.PageMove>
     </S.PageMoveHeaderWrapper>
   );
 };
