430e27f9d234311a592b50384d924bdada77d91e
Kimjimin0807 <jm004471@naver.com>
2021-10-06 16:48:09 +0900

    ⚡️ :: assest 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,6 @@
-export { default as CameraImg } from './clubEstblImg/Camera.svg';
-export { default as GpsIcon } from './clubEstblImg/GpsIcon.svg';
-export { default as Logo } from './header/Logo.svg';
-export { default as Bell } from './header/Bell.svg';
-export { default as List } from './header/List.svg';
+export { default as CameraImg } from "./clubEstblImg/Camera.svg";
+export { default as GpsIcon } from "./clubEstblImg/GpsIcon.svg";
+export { default as Logo } from "./header/Logo.svg";
+export { default as Bell } from "./header/Bell.svg";
+export { default as List } from "./header/List.svg";
+export { default as LocationIcon } from "./detailClub/LocationIcon.svg";
