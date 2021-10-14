0530cbfb719ba858e6b2dab8fd6d0b29f85552f4
Kimjimin0807 <jm004471@naver.com>
2021-10-14 17:17:57 +0900

    ⚡️ :: 매칭 등록 컨테이너 추가

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,2 +1,3 @@
-export {default as ClubManagementContainer} from './clubManagement';
-export {default as SignupContainer} from './signup/Signup'
+export { default as ClubManagementContainer } from "./clubManagement";
+export { default as SignupContainer } from "./signup/Signup";
+export { default as MatchingRegistrationContainer } from "./matchingRegistration";
