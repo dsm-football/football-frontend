bc370dbbe7d3f0e8d7bf2a928dad952138ca6dc5
forever7city <wlsgur0324@naver.com>
2021-10-14 14:03:11 +0900

    ⚡️ :: MyPage 경로설정 추가

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { default as ClubManagement } from "./clubManagement/ClubManagement";
 export { default as ClubEstbl } from "./ClubEstbl/ClubEstblCompletion";
 export { default as Login } from "./login/Login";
 export { default as MainPage } from "./main/main";
+export { default as MyPage } from "./myPage/Mypage"
