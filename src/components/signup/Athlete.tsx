b2bbcf5fdcd834eece7174889e681327ef4f8d2e
LeeJinWoo <wlsdn5793@gmail.com>
2021-10-16 22:23:29 +0900

    ⚡️ :: 회원가입 redux 추가

--- src/components/signup/Athlete.tsx
+++ src/components/signup/Athlete.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setAthlete, setPage } from "../../modules/redux/action/signup";
 import * as S from "./style";
 const Position = () => {
+  const dispatch = useDispatch();
+  const dispatchAthlete = (athlete: boolean) => {
+    dispatch(setAthlete(athlete));
+    dispatch(setPage(5));
+  };
   return (
     <>
       <S.SignupWrapper>
         <S.BigCardWrapper>
-          <S.SignupCard>선수출신</S.SignupCard>
-          <S.SignupCard>비선수출신</S.SignupCard>
+          <S.SignupCard onClick={() => dispatchAthlete(true)}>
+            선수출신
+          </S.SignupCard>
+          <S.SignupCard onClick={() => dispatchAthlete(false)}>
+            비선수출신
+          </S.SignupCard>
         </S.BigCardWrapper>
       </S.SignupWrapper>
     </>
