baae1bf6f3ccd322356860a5b2e6311e06400e3a
Kimjimin0807 <jm004471@naver.com>
2021-10-20 23:20:07 +0900

    ⚡️ :: asset 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,3 +5,6 @@ export { default as Google } from "./login/Google.svg";
 export { default as Logo } from "./header/Logo.svg";
 export { default as Bell } from "./header/Bell.svg";
 export { default as List } from "./header/List.svg";
+
+export { default as PrevIcon } from "./calendar/PrevIcon.svg";
+export { default as NextIcon } from "./calendar/NextIcon.svg";
