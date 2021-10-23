fec867fc176d800068a708a0ae0fa6c32410eb33
Kimjimin0807 <jm004471@naver.com>
2021-10-23 21:49:46 +0900

    ⚡️ :: setDate 삭제

--- src/util/hooks/calendar/useCalendar.tsx
+++ src/util/hooks/calendar/useCalendar.tsx
@@ -1,15 +1,11 @@
-import moment from "moment";
 import { useDispatch } from "react-redux";
-import { setCalendar, setDate } from "../../../modules/redux/action/calendar";
+import { setCalendar } from "../../../modules/redux/action/calendar";
 import { useSelectState } from "../default";
 
 const useCalendar = () => {
   const dispatch = useDispatch();
   const state = useSelectState().calendar;
   const setState = {
-    setDate: (payload: moment.Moment) => {
-      dispatch(setDate(payload));
-    },
     setCalendar: (payload: boolean) => {
       dispatch(setCalendar(payload));
     },
