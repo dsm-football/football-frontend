3f7fb8d87f7735943ba952a8aff7d5302f61928d
LeeJinWoo <wlsdn5793@gmail.com>
2021-10-27 15:51:49 +0900

    ⚡️ :: 동호회 개설 페이지 action, reducer 작업 완료

--- src/modules/redux/reducer/index.ts
+++ src/modules/redux/reducer/index.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import { userSignupreducer } from "./signup";
+import { clubEstblreducer} from './clubEstbl';
 const rootReducer = combineReducers({
-    userSignupreducer
+    userSignupreducer,
+    clubEstblreducer
 });
 
 export type reducerType = ReturnType<typeof rootReducer>;
