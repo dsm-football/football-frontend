bbe2139164f3f8c523c716733e4f03d9998e3bfe
Kimjimin0807 <jm004471@naver.com>
2021-11-13 23:49:37 +0900

    ⚡️ :: 클럽 관리 api 연동

--- src/containers/clubManagement/ClubManagementContainer.tsx
+++ src/containers/clubManagement/ClubManagementContainer.tsx
@@ -1,8 +1,21 @@
-import React, { FC, Suspense } from "react";
+import React, { FC, Suspense, useEffect } from "react";
+import { useParams } from "react-router";
 import { ClubManagement } from "../../components";
 import { Header } from "../../components/common";
+import UseClubManagement from "../../util/hooks/clubManagement";
 
 const ClubManagementContainer: FC = () => {
+  const { state, setState } = UseClubManagement();
+  const { id } = useParams() as any;
+
+  useEffect(() => {
+    setState.setId(id);
+  }, [id]);
+
+  useEffect(() => {
+    setState.getMemberList(id);
+  }, [state.id]);
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Header />
