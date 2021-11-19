44ae6ef4e7def4a3456a22d1d9b203d00145b90e
Kimjimin0807 <jm004471@naver.com>
2021-11-20 03:14:06 +0900

    ⚡️ :: 승인 대기중인 멤버 목록 및 동호회 정보 api 추가

--- src/util/api/clubManagement/index.ts
+++ src/util/api/clubManagement/index.ts
@@ -1,10 +1,29 @@
-import { getRequest } from "../default";
+import { getRequest, getRequestWithAccessToken } from "../default";
 import uri from "../uri";
 
 export const getClubMember = async (id: string) => {
   try {
     const request = getRequest();
     return await request.get(uri.club + id + "/member");
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getApplicationMember = async () => {
+  try {
+    const accessToken = localStorage.getItem("access_token") || "";
+    const request = getRequestWithAccessToken(accessToken);
+    return await request.get(uri.application);
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getClubInfo = async (id: string) => {
+  try {
+    const request = getRequest();
+    return await request.get(uri.club + id);
   } catch (error) {
     throw error;
   }
