6ac90c00c27825438b478eed68e50f43ba6cdff6
Kimjimin0807 <jm004471@naver.com>
2021-11-21 23:54:16 +0900

    ⚡️ :: 매칭등록 api 추가

--- src/util/api/matchRegistration/index.ts
+++ src/util/api/matchRegistration/index.ts
@@ -6,9 +6,33 @@ export const postMatchingRegistration = async (
   accessToken: string,
   matchOption: MatchOptionType
 ) => {
+  const {
+    gameType,
+    area,
+    matchDate,
+    endDate,
+    person,
+    age,
+    gender,
+    hasReferee,
+    longitude,
+    latitude,
+    matchTime,
+  } = matchOption;
   try {
     const request = getRequestWithAccessToken(accessToken);
-    const response = await request.post(uri.matchingRegistration, matchOption);
+    const response = await request.post(uri.matchingRegistration, {
+      game_type: gameType,
+      area: area,
+      date: `${matchDate}T${matchTime}:00`,
+      end_date: endDate,
+      personnel: person,
+      age_type: age,
+      gender_type: gender,
+      gas_referee: hasReferee,
+      longitude: longitude || 0,
+      latitude: latitude || 0,
+    });
     return response;
   } catch (error) {
     throw error;
