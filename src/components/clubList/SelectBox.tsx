4b32deb14eb5d159fa71afb261010e6a5a0fa0a4
forever7city <wlsgur0324@naver.com>
2021-11-23 16:18:54 +0900

    :memo: :: 동호회 리스트 코드 수정

--- src/components/clubList/SelectBox.tsx
+++ src/components/clubList/SelectBox.tsx
@@ -1,39 +1,6 @@
 import React from 'react'
 import * as S from './style';
-
-const area = [
-    "전체",
-    "서울",
-    "부산",
-    "대구",
-    "인천",
-    "광주",
-    "대전",
-    "울산",
-    "세종",
-    "경기",
-    "강원",
-    "충북",
-    "충남",
-    "전북",
-    "전남",
-    "경북",
-    "경남",
-    "제주",
-  ];
-
-const status = [
-    "전체",
-    "모집중",
-    "끝남"
-];
-
-const gender = [
-    "전체",
-    "남자",
-    "여자",
-    "혼성"
-];
+import { areaOption, status, ageOption, genderOption } from '../../constance/option';
 
 
 const Select = () => {
@@ -43,7 +10,8 @@ const Select = () => {
                 <S.SelectBox>
                     <S.Defulat>지역</S.Defulat>
                     <S.Choice>
-                        {area.map((v: string, i: number) => {
+                        <option>전체</option>
+                        {areaOption.map((v: string, i: number) => {
                         return (
                         <option key={i} value={v}>
                             {v}
@@ -68,15 +36,20 @@ const Select = () => {
                 <S.Defulat>나이</S.Defulat>
                     <S.Choice>
                         <option>전체</option>
-                        {[...Array(7)].map((v: any, i: number) => {
-                        return <option key={i}>{(i + 1) * 10}대</option>;
+                        {ageOption.map((v: string, i: number) => {
+                            return (
+                            <option key={i} value={v}>
+                            {v}
+                            </option>
+                            );
                         })}
                     </S.Choice>
                 </S.SelectBox>
                 <S.SelectBox>
                 <S.Defulat>성별</S.Defulat>
                     <S.Choice>
-                        {gender.map((v: string, i: number) => {
+                        <option>전체</option>
+                        {genderOption.map((v: string, i: number) => {
                         return (
                         <option key={i} value={v}>
                             {v}
