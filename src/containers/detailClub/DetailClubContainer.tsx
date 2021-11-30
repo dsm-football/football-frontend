12ec3cf13467ab4222a29fae39976071f05c0af2
Kimjimin0807 <jm004471@naver.com>
2021-11-30 11:30:59 +0900

    ⚡️ :: 동호회 상세보기 api 연동

--- src/containers/detailClub/DetailClubContainer.tsx
+++ src/containers/detailClub/DetailClubContainer.tsx
@@ -1,8 +1,22 @@
-import React, { FC, Suspense } from "react";
+import React, { FC, Suspense, useEffect } from "react";
+import { useParams } from "react-router";
 import { Header } from "../../components/common";
 import DetailClub from "../../components/detailClub/DetailClub";
+import UseClubInfo from "../../util/hooks/clubInfo";
 
 const DetailClubContainer: FC = () => {
+  const { state, setState } = UseClubInfo();
+  const { id } = useParams() as any;
+
+  useEffect(() => {
+    setState.setId(id);
+  }, [id]);
+
+  useEffect(() => {
+    setState.getMemberList();
+    setState.getClubInfo();
+  }, [state.id]);
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Header />
