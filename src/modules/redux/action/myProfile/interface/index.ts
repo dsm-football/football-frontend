b74c846e2c16a136fae085f13340d134ef858758
Kimjimin0807 <jm004471@naver.com>
2021-12-04 15:58:04 +0900

    ⚡️ :: myprofile action 추가

---
+++ src/modules/redux/action/myProfile/interface/index.ts
@@ -0,0 +1,4 @@
+export const GET_MY_PROFILE = "USER/MY_PROFILE" as const;
+export const GET_MY_PROFILE_SUCCESS = "USER/MY_PROFILE_SUCCESS" as const;
+export const GET_MY_PROFILE_FAILURE = "USER/MY_PROFILE_FAILURE" as const;
+export const SET_ID = "USER/SET_ID" as const;
