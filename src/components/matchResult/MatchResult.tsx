1cbe8dd2de003dc5432ccf3c04b20681393ffa6f
Kimjimin0807 <jm004471@naver.com>
2021-12-04 23:13:57 +0900

    ⚡️ :: 경기결과 등록 컴포넌트 수정

--- src/components/matchResult/MatchResult.tsx
+++ src/components/matchResult/MatchResult.tsx
@@ -1,13 +1,67 @@
-import React from "react";
+import React, { FC, useState } from "react";
+import { useHistory } from "react-router";
+import { VSIcon } from "../../assets";
+import { postGameResult } from "../../util/api/matchResult";
 import { PageMoveHeader } from "../common";
-import MatchScore from "./matchScore/MatchScore";
 import * as S from "./style";
 
-const MatchResult = () => {
+const MatchResult: FC<any> = (props) => {
+  const [hostScore, setHostScore] = useState<number | undefined>();
+  const [participantScore, setParticipantScore] = useState<
+    number | undefined
+  >();
+  const history = useHistory();
+
+  const onSubmitResult = () => {
+    if (hostScore && participantScore) {
+      postGameResult(Number(props.id), hostScore, participantScore)
+        .then(() => {
+          alert("등록 성공");
+          history.goBack();
+        })
+        .catch(() => {
+          alert("잠시 후 시도해주세요");
+          history.goBack();
+        });
+    } else {
+      alert("스코어를 입력해주세요.");
+    }
+  };
+
   return (
     <>
       <PageMoveHeader header="경기 결과 등록 페이지" />
-      <MatchScore />
+      <S.MainWrapper>
+        <S.ScoreWrapper>
+          <div className="scoreInput">
+            <input
+              type="number"
+              placeholder="00"
+              value={hostScore || ""}
+              onChange={(e) => {
+                setHostScore(Number(e.target.value));
+              }}
+            />
+            <span>우리팀</span>
+          </div>
+          <img src={VSIcon} alt="VS" />
+          <div className="scoreInput">
+            <input
+              type="number"
+              placeholder="00"
+              value={participantScore || ""}
+              onChange={(e) => {
+                setParticipantScore(Number(e.target.value));
+              }}
+            />
+            <span>상대팀</span>
+          </div>
+        </S.ScoreWrapper>
+        <span className="subFont">얼마나 득점했나요?</span>
+        <button className="checkBtn" onClick={onSubmitResult}>
+          확인
+        </button>
+      </S.MainWrapper>
     </>
   );
 };
