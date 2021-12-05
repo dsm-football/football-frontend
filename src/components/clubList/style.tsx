55725084ceb11d916a70207f76bb1243e89eccea
LeeJinWoo <wlsdn5793@gmail.com>
2021-12-06 07:38:20 +0900

    수정

--- src/components/clubList/style.tsx
+++ src/components/clubList/style.tsx
@@ -1,183 +1,183 @@
 import styled from "styled-components";
 
 export const Title = styled.div`
-    width: 200px;
-    height: 28px;
-    margin: 20px 20px;
-    font-weight: 600;
-    font-size: 20px;
-    line-height: 28px;
-`
+  width: 200px;
+  height: 28px;
+  margin: 20px 20px;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 28px;
+`;
 
 export const Divide = styled.div`
-    width: 100%;
-    height: 4px;
-    background-color: #F3F3F3;
-`
+  width: 100%;
+  height: 4px;
+  background-color: #f3f3f3;
+`;
 
 export const Find = styled.div`
-    width: 100%;
-    height: 35px;
-    padding: 8px 20px;
-    display: flex;
-    justify-content: space-between;
-    text-align: center;
-`
+  width: 100%;
+  height: 35px;
+  padding: 8px 20px;
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+`;
 
 export const All = styled.div`
-    width: 60px;
-    height: 20px;
-    font-size: 14px;
-    line-height: 20px;
-`
+  width: 60px;
+  height: 20px;
+  font-size: 14px;
+  line-height: 20px;
+`;
 
 export const Period = styled.select`
-    width: 80px;
-    height: 20px;
-    border: none;
-    cursor: pointer;
-    font-size: 14px;
-    color: #5B5B5B;
-`
+  width: 80px;
+  height: 20px;
+  border: none;
+  cursor: pointer;
+  font-size: 14px;
+  color: #5b5b5b;
+`;
 
 export const Options = styled.div`
-    width: 100%;
-    height: 60px;
-    background-color: #F5F5F5;
-    display: flex;
-    overflow-x:scroll;
-    padding: 10px 20px;
-`
+  width: 100%;
+  height: 60px;
+  background-color: #f5f5f5;
+  display: flex;
+  overflow-x: scroll;
+  padding: 10px 20px;
+`;
 
 export const SelectBox = styled.div`
-    width: 120px;
-    height: 40px;
-    background-color: white;
-    display: flex;
-    justify-content: space-evenly;
-    text-align: center;
-    margin-right: 10px;
-`
+  width: 120px;
+  height: 40px;
+  background-color: white;
+  display: flex;
+  justify-content: space-evenly;
+  text-align: center;
+  margin-right: 10px;
+`;
 
 export const Defulat = styled.div`
-    width: 30px;
-    height: 20px;
-    font-size: 14px;
-    line-height: 20px;
-    margin-top: 10px;
-`
+  width: 30px;
+  height: 20px;
+  font-size: 14px;
+  line-height: 20px;
+  margin-top: 10px;
+`;
 
 export const Choice = styled.select`
-    width: 55px;
-    height: 25px;
-    border: none;
-    margin-top: 7px;
-    font-size: 14px;
-    line-height: 20px;
-    color: #5B5B5B;
-    cursor: pointer;
-`
+  width: 55px;
+  height: 25px;
+  border: none;
+  margin-top: 7px;
+  font-size: 14px;
+  line-height: 20px;
+  color: #5b5b5b;
+  cursor: pointer;
+`;
 
 export const Clubs = styled.div`
-    width: 320px;
-    height: 140px;
-    border: 1px solid #EFEFEF;
-    border-radius: 10px;
-    margin-top: 12px;
-    margin-left: 20px;
-    text-align: center;
-`
+  width: 320px;
+  height: 140px;
+  border: 1px solid #efefef;
+  border-radius: 10px;
+  margin-top: 12px;
+  margin-left: 20px;
+  text-align: center;
+`;
 
 export const Explain = styled.div`
-    width: 70%;
-    height: 70px;
-    display: flex;
-    justify-content: space-evenly;
-    flex-direction: column;
-`
+  width: 70%;
+  height: 70px;
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: column;
+`;
 
 export const Top = styled.div`
-    width: 100%;
-    height: 70px;
-    display: flex;
-    justify-content: space-evenly;
-`
+  width: 100%;
+  height: 70px;
+  display: flex;
+  justify-content: space-evenly;
+`;
 
-export const ClubImage = styled.div`
-    width: 45px;
-    height: 45px;
-    margin-top: 20px;
-    border-radius: 50%;
-    background-color: black;
-`
+export const ClubImage = styled.img`
+  width: 45px;
+  height: 45px;
+  margin-top: 20px;
+  border-radius: 50%;
+  background-color: black;
+`;
 
 export const ClubName = styled.div`
-    width: 240px;
-    height: 20px;
-    margin-top: 20px;
-    font-weight: bold;
-    font-size: 14px;
-    line-height: 20px;
-    color: #121212;
-`
+  width: 240px;
+  height: 20px;
+  margin-top: 20px;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 20px;
+  color: #121212;
+`;
 
 export const ClubExplain = styled.div`
-    width: 240px;
-    height: 16px;
-    font-size: 12px;
-    line-height: 16px;
-    color: #161616;
-`
+  width: 240px;
+  height: 16px;
+  font-size: 12px;
+  line-height: 16px;
+  color: #161616;
+`;
 
 export const Match = styled.div`
-    width: 160px;
-    height: 15px;
-    margin-top: 10px;
-    margin-left: 12px;
-    font-size: 11px;
-    line-height: 15px;
-    color: #828282;
-`
+  width: 160px;
+  height: 15px;
+  margin-top: 10px;
+  margin-left: 12px;
+  font-size: 11px;
+  line-height: 15px;
+  color: #828282;
+`;
 
 export const Selected = styled.div`
-    width: 100%;
-    height: 40px;
-    margin-top: 10px;
-    display: flex;
-    justify-content: space-evenly;
-`
+  width: 100%;
+  height: 40px;
+  margin-top: 10px;
+  display: flex;
+  justify-content: space-evenly;
+`;
 
 export const BoxDiv = styled.div`
-    width: 50%;
-    height: 40px;
-    display: flex;
-    justify-content: space-evenly;
-`
+  width: 50%;
+  height: 40px;
+  display: flex;
+  justify-content: space-evenly;
+`;
 
 export const Box = styled.div`
-    width: 40px;
-    height: 20px;
-    background-color: #F0F0F0;
-    border-radius: 2px;
-    padding: 3px 6px;
-    font-weight: 500;
-    font-size: 11px;
-    line-height: 13px;
-`
+  width: 40px;
+  height: 20px;
+  background-color: #f0f0f0;
+  border-radius: 2px;
+  padding: 3px 6px;
+  font-weight: 500;
+  font-size: 11px;
+  line-height: 13px;
+`;
 
 export const Recruit = styled.div`
-    widtH: 75px;
-    height: 24px;
-    background-color: #0BFF0B;
-    border-radius: 50px;
-    font-weight: 500;
-    font-size: 11px;
-    line-height: 13px;
-    padding: 5px 8px;
-`
+  width: 75px;
+  height: 24px;
+  background-color: #0bff0b;
+  border-radius: 50px;
+  font-weight: 500;
+  font-size: 11px;
+  line-height: 13px;
+  padding: 5px 8px;
+`;
 
 export const Poeple = styled(Recruit)`
-    width: 50px;
-    font-size: 12px;
-    line-height: 14px;
-`
+  width: 50px;
+  font-size: 12px;
+  line-height: 14px;
+`;
