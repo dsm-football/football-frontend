9357e8ddaa9baccca2aafb3b7a27ebea3590ba2d
forever7city <wlsgur0324@naver.com>
2021-12-05 20:33:14 +0900

    :memo: :: MyPage 연동코드 수정작업 진행중

--- src/components/myPage/Mypage.tsx
+++ src/components/myPage/Mypage.tsx
@@ -1,56 +1,50 @@
-import React, {useEffect, useState, ChangeEventHandler} from 'react'
-import * as S from './style';
-import Header from "../common/header/Header"
-import UserExplain from './funciton';
-import { MyPageToken } from '../../util/api/mypage';
+import React, { useEffect, useState, ChangeEventHandler } from "react";
+import * as S from "./style";
+import Header from "../common/header/Header";
+import UserExplain from "./funciton";
+import { MyPageToken } from "../../util/api/mypage";
+import UseMyProfile from "../../util/hooks/myProfile";
 
 const Mine = () => {
-    useEffect(() =>{
-       MyPageToken().then((res) => {
-           console.log(res);
-       }).catch((e) => {
-           console.log(e);
-       })
-    }, [])
+  const { state, setState } = UseMyProfile();
+  const [isModifyMode, setIsModifyMode] = useState(false);
+  const { profileInfo } = state;
 
-    const [isModifyMode, setIsModifyMode] = useState(false);
+  useEffect(() => {
+    setState.setMyProfile();
+  }, [state.id]);
 
-    const [form, setForm] = useState({
-        '구글 계정': '',
-        '동호회 이름': '',
-        '포지션': '',
-        '지역': '',
-        '나이': '',
-        '나의 한줄 소개': '',
-    });
+  const updateForm: ChangeEventHandler = (e) => {
+    const $target = e.target as HTMLInputElement;
+  };
 
-    const updateForm: ChangeEventHandler = (e) => {
-        const $target = e.target as HTMLInputElement;
-        setForm(prev => ({ ...prev, [$target.name]: $target.value }));
-    }
+  const Modify = () => {
+    if (!isModifyMode) return setIsModifyMode(true);
+    setIsModifyMode(false);
+  };
 
-    const Modify = () => {
-        if (!isModifyMode) return setIsModifyMode(true);
-        setIsModifyMode(false);
-        console.log(form);
-    }
-
-    return(
-        <>
-            <Header />
-            <S.Modify onClick={Modify}>{isModifyMode ? '수정완료' : '수정하기'}</S.Modify>
-            <S.Div>
-                <S.Profile></S.Profile>
-                <div>
-                    <S.Name>김수인</S.Name>
-                    <S.Player>선수출신</S.Player>
-                </div>
-            </S.Div>
-            <S.Div>
-                <UserExplain user={form} modifier={updateForm} isDisabled={!isModifyMode}/>
-            </S.Div>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <S.Modify onClick={Modify}>
+        {isModifyMode ? "수정완료" : "수정하기"}
+      </S.Modify>
+      <S.Div>
+        <S.Profile></S.Profile>
+        <div>
+          <S.Name>{profileInfo.name}</S.Name>
+          <S.Player>{profileInfo.is_pro ? "선수출신" : "비선수출신"}</S.Player>
+        </div>
+      </S.Div>
+      <S.Div>
+        <UserExplain
+          user={profileInfo}
+          modifier={updateForm}
+          isDisabled={!isModifyMode}
+        />
+      </S.Div>
+    </>
+  );
 };
 
 export default Mine;
--- src/components/myPage/funciton.tsx
+++ src/components/myPage/funciton.tsx
@@ -1,52 +1,41 @@
-import React, { FC, useEffect } from 'react';
-import * as S from './style';
-
-type UserType = {
-        '구글 계정': string;
-        '동호회 이름': string;
-        '포지션': string;
-        '지역': string;
-        '나이': string;
-        '나의 한줄 소개': string;
-    };
+import React, { FC, useEffect } from "react";
+import { MyProfileResponseType } from "../../constance/myProfile";
+import * as S from "./style";
 
 type UserExplainProps = {
-    user: UserType;
-    isDisabled: boolean;
-    modifier: React.ChangeEventHandler<HTMLInputElement>;
-}
+  user: MyProfileResponseType;
+  isDisabled: boolean;
+  modifier: React.ChangeEventHandler<HTMLInputElement>;
+};
 
-const UserExplain:FC<UserExplainProps> = ({ user, isDisabled ,modifier }) => {
-    const arr1 = Object.keys(user);
-    const arr2 = Object.values(user);
-    
-    return(
-        <>
-            <S.Divs>
-                <S.Line>
-                    {arr1.map((el, index) => (
-                        <div key={index}>
-                            {el}
-                        </div>
-                    ))}
-                </S.Line>
-                <S.Default>
-                    {arr2.map((el, index) => (
-                        <input
-                            placeholder={arr1[index] + " 입력"}
-                            onChange={modifier}
-                            disabled={isDisabled}
-                            key={index}
-                            name={arr1[index]}
-                        />
-                    ))}
-                </S.Default>
-            </S.Divs> 
-        </>
-    );
-}
+const UserExplain: FC<UserExplainProps> = ({ user, isDisabled, modifier }) => {
+  const arr1 = Object.keys(user);
+  const arr2 = Object.values(user);
 
-
-
+  return (
+    <>
+      <S.Divs>
+        <S.Line>
+          {arr1.map((el, index) => (
+            <div key={index}>{el}</div>
+          ))}
+        </S.Line>
+        <S.Default>
+          {arr2.map((el, index) => {
+            return (
+              <input
+                placeholder={arr1[index] + " 입력"}
+                onChange={modifier}
+                disabled={isDisabled}
+                key={index}
+                name={arr1[index]}
+              />
+            );
+          })}
+        </S.Default>
+      </S.Divs>
+    </>
+  );
+};
 
 export default UserExplain;
